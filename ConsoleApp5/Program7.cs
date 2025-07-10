77052e8a3be9afaad3f7ea908aad86766d34262c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-10 23:26:50 +0330

    add enum

---
+++ ConsoleApp5/Program7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApplication
+{
+    enum Level
+    {
+        Low,
+        Medium,
+        High,
+    }
+    class Program
+    {
+       public static void MyProgram()
+        {
+            Level level = Level.Low;
+            Console.WriteLine(level);
+        }
+    }
+}
