ae7ddb9b7cfe45cdea223173c0f4d941e71ccd39
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-10 23:09:40 +0330

    add person

---
+++ ConsoleApp5/Program6.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; } 
+        }
+    }
+}
