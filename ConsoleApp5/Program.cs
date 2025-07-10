969e93eff348b0f0056ff3cedfcaf9fa3f216e49
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-10 23:26:43 +0330

    update enum level

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -16,6 +16,7 @@ namespace MyApplication
             Person person = new Person();
             person.Name = "Arezou";
             Console.WriteLine(person.Name);
+            Program.MyProgram();
         }
     }
 }
